15051675a767e24d818d9d7544be44b095c2c107
Polina Gurina <p.gurina@icloud.com>
2023-11-17 17:37:06 +0100

    Switch to altool

---
+++ scripts/deploy-to-testflight.js
@@ -0,0 +1,16 @@
+const { execSync } = require("child_process")
+const packJson = require("../package.json")
+const path = require("path")
+
+function main() {
+    const buildPath = path.resolve(
+        __dirname,
+        "../out/make/",
+        `${packJson.productName}-${packJson.version}-x64.pkg`
+    )
+    execSync(
+        `xcrun altool --upload-app -f ${buildPath} -t macos --apiKey ${process.env.APP_STORE_API_KEY_ID} --apiIssuer ${process.env.APP_STORE_API_ISSUER_ID}`
+    )
+}
+
+main()
